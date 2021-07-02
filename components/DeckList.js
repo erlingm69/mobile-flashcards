8da00583d8a5b5761161d7f602be212d783b3b4f
Erling Mårtensson <erling.martensson@gmail.com>
2021-07-02 12:00:28 +0200

    feature: Dummy list

---
+++ components/DeckList.js
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+
+export default function DeckList() {
+    return(
+        <View>
+        <Text>Deck List</Text>
+        </View>
+    )
+}
