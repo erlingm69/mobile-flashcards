c2e19b75b6c8dc038e58d09d503a4d04b14a8df2
Erling Mårtensson <erling.martensson@gmail.com>
2021-07-03 11:19:41 +0200

    feature: Show deck details

--- components/Deck.js
+++ components/Deck.js
@@ -1,17 +1,39 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { connect } from 'react-redux'
+import { gray } from '../utils/colors'
 
-function Deck(props) {
+function Deck({title, questions}) {
     return (
-        <View><Text>Deck</Text>
+        <View style={styles.container}>
+            <Text style={styles.titleText}>{title}</Text>
+            <Text style={styles.subTitleText}>{`${questions.length} cards`}</Text>
         </View>
     )
 }
 
-function mapStateToProps(state) {
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    }, titleText: {
+        marginTop: 10,
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    subTitleText: {
+        fontSize: 15,
+        textAlign: "center",
+        color: gray
+    },
+
+  });  
+
+function mapStateToProps(state, {route}) {
+    const { deckId } = route.params
     return {
-        decks: state
+        deckId,
+        ...state[deckId]
     }
 }
 
